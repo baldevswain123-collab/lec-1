6e8c18d4f2455e1bef7d46f01845ea013b78eb52
baldevswain123-collab <baldevswain123@gmail.com>
2026-07-21 08:57:39 +0530

    User Interaction and Operators

---
+++ 04 javascript/lec 3/user interaction.js
@@ -0,0 +1,28 @@
+//alert("Hello Everyone!")
+//window.alert("Good morning")
+
+//let name = prompt("Enter your name:-")
+//console.log(name)
+
+
+//let name = prompt("Enter your name:-")
+//if (name) {
+  //  console.log("Baldev", name);
+    
+//} else {
+  //  console.log("Re Enter user name");
+    
+    //console.log("name is" , prompt("Re Enter User name"));   
+//}
+
+//let choice = confirm("Are you enjoying js classes?")
+//console.log(choice);
+
+console.log("Hii"); //nornmal
+console.warn("hello"); //yellow
+console.error("bye"); //red
+
+
+
+
+
