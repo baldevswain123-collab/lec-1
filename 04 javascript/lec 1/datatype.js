0a5301919f720c3b0cbc5fc5c2a4c7d1f2d19c0b
baldevswain123-collab <baldevswain123@gmail.com>
2026-08-22 09:17:40 +0530

    Variable and Constant

--- 04 javascript/lec 1/datatype.js
+++ 04 javascript/lec 1/datatype.js
@@ -1,26 +1,26 @@
-console.log(typeof 10)
-console.log(typeof 10.2)
-console.log(typeof NaN)
+// console.log(typeof 10)
+// console.log(typeof 10.2)
+// console.log(typeof NaN)
 
-console.log(typeof "10")
-console.log(typeof "hello")
+// console.log(typeof "10")
+// console.log(typeof "hello")
 
-console.log(typeof true)
-console.log(typeof false)
+// console.log(typeof true)
+// console.log(typeof false)
 
-console.log(typeof undefined)
+// console.log(typeof undefined)
 
-console.log(typeof null)
+// console.log(typeof null)
 
-console.log(typeof Symbol(10))
+// console.log(typeof Symbol(10))
 
-console.log(typeof BibInt(23))
+// console.log(typeof BibInt(23))
 
-let a ;
-console.log(a)
+// let a ;
+// console.log(a)
 
-console.log(b)
-var b = 20
+// console.log(b)
+// var b = 20
 
-let h4 = document.querySelector("h4")
-console.log(h4)
+// let h4 = document.querySelector("h4")
+// console.log(h4)
