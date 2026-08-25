dcd471ed010118a26cf80ba8c7897f44e10ab0ad
baldevswain123-collab <baldevswain123@gmail.com>
2026-08-25 18:24:35 +0530

    User Interaction and Operator

---
+++ 04 javascript/lec 3 - User Interaction and Operators/operators.js
@@ -0,0 +1,45 @@
+//console.log(2**3);
+//nsole.log(9**3);
+
+//let x = 10
+//x **= 2 x = x ** 2
+//console.log(x);
+
+//console.log(5 == "5");
+//console.log(5 == "4");
+//console.log(1 == true);
+//console.log(0 == false);
+
+
+
+//console.log(5 === "5");
+//console.log(1 === true);
+//console.log(0 === false);
+
+//console.log(3 != 5);
+//console.log(3 != 3);
+
+//console.log(x << y);
+//x * 2^y
+//console.log(4 << 3);
+
+//console.log(x >> y);
+//x / 2^y
+//console.log(64 >> 5);
+//console.log(256 >> 3);
+
+// >>> (unsigned right shift)
+
+let x = 10 , y= 20
+console.log(x > y ? x : y);
+
+ 
+
+
+
+
+
+
+
+
+
