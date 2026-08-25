8b58032b1320e7578b6dabffcb2c272d796cf71a
baldevswain123-collab <baldevswain123@gmail.com>
2026-08-25 18:20:17 +0530

    Intro to JS - Datatypes , Constant , Variable

---
+++ 04 javascript/lec 1 - Intro to JS/diff bet var let & const.js
@@ -0,0 +1,56 @@
+ // function myFun() {
+//   let var1 = 10;
+//   const var2 = 20;
+//   var var3 = 30
+// }
+
+// myFun()
+
+// // console.log(var1)
+// // console.log(var2)
+// // console.log(var3)
+
+
+// ============ DT - 20 7 26
+// i)
+// {
+//   let a = 10;
+//   const b = 20;
+//   var c = 30
+// }
+
+// function fun() {
+//   debugger
+//   let a1 = 10;
+//   const b1 = 20;
+//   var c1 = 30
+
+//   console.log("hiiiiiii")
+// }
+
+// fun()
+
+
+// // ii)
+// const name = "Sai"
+// console.log("Before reassign", name)
+// name = 'Pranab'
+// console.log('After reassign', name)
+
+
+// iii)
+// const instituteName = "JT"
+// console.log("instituteName-------", instituteName)
+// const instituteName = "Java Technocrat"
+// console.log("instituteName=======", instituteName)
+
+// iv)
+console.log(x)
+console.log(y)
+
+//var x = 10
+// let y = 20;
+//const z = 30
+
+
+
